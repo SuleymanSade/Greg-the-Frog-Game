148cbc23d107cc2f3530c1e5101fe3fe84980186
Suleyman Sade <suleymansade09@gmail.com>
2026-06-20 07:27:40 -0700

    working functions

--- data_transfer.py
+++ data_transfer.py
@@ -10,35 +10,40 @@ ser = serial.Serial(port = PORT, baudrate=SERIAL_RATE, timeout=1)
 
 def get_data():
     # Set: x-val, y-val, hearth rate
-    clean = "-1, -1, -1" # TODO: put the number of commmas for the default val
+    clean = "-1, -1" # TODO: put the number of commmas for the default val
 
     raw = ser.readline()
 
-    while raw:
+    if raw:
         # raw = ser.read_all()
         clean = raw.decode('utf-8', errors='ignore').strip()
-        print(clean)
+        # print(clean)
     data = clean.split(", ")
 
-    # for i in range(1):
-    #     if data[i] == '-1':
-    #         data[i] = None
-    #     else:
-    #         data[i] = int(data[i])
+    for i in range(1):
+        if data[i] == '-1':
+            data[i] = None
+        elif data[i] == '':
+            data[i] = 0
+        else:
+            data[i] = int(data[i])
+        # print(data[i], end=", ")
 
     return data
 
 
 def detect_flick():
-    motion = []
+    # if type(allHistory[-1][0]) is str or type(allHistory[-2][0]) is str or type(allHistory[-1][1]) is str or type(allHistory[-2][1]) is str:
+    #     return None
+    motion = [0,0]
     isExtX = False
     isExtY = False
 
-    if allHistory[-1][0] < 550 and allHistory[-1][0] > 450:
-        if allHistory[-2][0] > 550 or allHistory[-2][0] < 450:
+    if int(allHistory[-1][0]) < 550 and int(allHistory[-1][0]) > 450:
+        if int(allHistory[-2][0]) > 550 or int(allHistory[-2][0]) < 450:
             isExtX = True
-    if allHistory[-1][1] < 550 and allHistory[-1][1] > 450:
-        if allHistory[-2][1] > 550 or allHistory[-2][1] < 450:
+    if int(allHistory[-1][1]) < 550 and int(allHistory[-1][1]) > 450:
+        if int(allHistory[-2][1]) > 550 or int(allHistory[-2][1]) < 450:
             isExtY = True
 
     deg = None
@@ -46,16 +51,18 @@ def detect_flick():
     if isExtX or isExtY:
         motion[0] = allHistory[-2][0]
         motion[1] = allHistory[-2][1]
-        deg = math.atan2(motion[0]-512, motion[1]-512)
+        deg = math.atan2(int(motion[0])-512, int(motion[1])-512)
     
-    return deg
+    return deg / (math.pi) * 180
     
 time.sleep(2)
 while True:
     data = get_data()
 
-    # if data[0] is not None and data[1] is not None:
-    #     allHistory.append(data)
+    if data[0] is not None and data[1] is not None:
+        allHistory.append(data)
 
-    # if len(allHistory) >= 2:
-    #     print(detect_flick())
+    if len(allHistory) >= 2:
+        flick = detect_flick()
+        if flick is not None:
+            print(flick/ (math.pi) * 180)
